7f2595be74d4fde60ccada45ad449a8db73e6805
caviri <carlosvivarrios@gmail.com>
2023-07-24 11:09:48 +0200

    Updated yml

---
+++ app/idf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Actions
+# A1 - Clone github
+git clone https://github.com/eqasim-org/ile-de-france.git /odtp/odtp-workdir/scenario
+cd /odtp/odtp-workdir/scenario
+git checkout 18aa1d4
+
+# A2 - Process configuration
+cp /odtp/odtp-volume/config.yml /odtp/odtp-workdir/scenario/config.yml
+ln -s /odtp/odtp-volume/data /odtp/odtp-workdir/data
+
+# A3 - Run the tool
+python3 -m synpp
+#python3 -m synpp /odtp/odtp-workdir/scenario/config.yml
+
+## Matsim
+#java -Xmx20G -cp ile_de_france_run.jar org.eqasim.ile_de_france.RunSimulation --config-path ile_de_france_config.xml
+
+# A4 - Take output and export it
+zip -r output.zip /odtp/odtp-workdir/output
+mv output.zip /odtp/odtp-volume/output.zip
+cp /odtp/odtp-workdir/log.txt /odtp/odtp-volume/log.txt
+
+# A5 - Kill Container
--- app/startup.sh
+++ app/startup.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
 
-# Arguments
-## By now just positional arguments
-## $1 github scenario
-## $2 config file to replace
-
-# Actions #
-# A1 - Clone github
-# git clone https://github.com/eqasim-org/ile-de-france.git /odtp/odtp-workdir/scenario
-# git reset --hard 18aa1d4
-# git checkout 18aa1d4
-git clone $1 /odtp/odtp-workdir/scenario #GITHUB
-git checkout $2 #COMMIT
-
-
-# A2 - Process configuration
-cp /odtp/odtp-volume/config.yml /odtp/odtp-workdir/scenario/config.yml
-ln -s /odtp/odtp-volume/data /odtp/odtp-workdir/data
-
-# A3 - Run the tool
-cd /odtp/odtp-workdir/scenario
-python3 -m synpp
-#python3 -m synpp /odtp/odtp-workdir/scenario/config.yml
-
-## Matsim
-#java -Xmx20G -cp ile_de_france_run.jar org.eqasim.ile_de_france.RunSimulation --config-path ile_de_france_config.xml
-
-# A4 - Take output and export it
-
-# A5 - Kill Container
+bash /odtp/odtp-app/idf.sh 2>&1 | tee /odtp/odtp-workdir/log.txt
