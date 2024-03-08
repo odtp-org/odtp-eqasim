77bbe2f60a86b7304a1d546bf5059ca39dbff1b1
caviri <carlosvivarrios@gmail.com>
2024-03-08 15:32:05 +0100

    Updated corsica and templates using {{}} for variables

--- app/app.sh
+++ app/app.sh
@@ -2,98 +2,105 @@
 if [ "$SCENARIO" == "IDF" ]; then
     echo "IDF SCENARIO."
     
-    # Actions
-    # A1 - Clone github
+    # Cloning Scenario
     git clone https://github.com/eqasim-org/ile-de-france.git /odtp/odtp-workdir/scenario
     cd /odtp/odtp-workdir/scenario
     git checkout fb1112d2a7d1817746be84413da584c391059ad1
 
-    # A2A - Prepare parameters & Config File
-    # Read placeholders and create config file from Environment  
+    # Preparing parameters & config file
+    # Reading placeholders and create config file from environment variables 
     if [ "$PIPELINE" == "Synthesis" ]; then
         echo "Running Synthesis PIPELINE"
         python3 /odtp/odtp-client/parameters.py /odtp/odtp-app/config_templates/config_synthesis_idf.yml /odtp/odtp-workdir/scenario/config.yml
+    
+        # Preparing input data folder
+        ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
+
     else
         echo "Running Matsim PIPELINE"
         python3 /odtp/odtp-client/parameters.py /odtp/odtp-app/config_templates/config_matsim_idf.yml /odtp/odtp-workdir/scenario/config.yml
+    
+        # Preparing input data folder
+        ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
+
+        # We need to copy output files from previous step in output folder
+        cp -r /odtp/odtp-input/eqasim-output/* /odtp/odtp-workdir/output
+    
     fi
 
-    # A2B - Prepare datafolder
-    ln -s /odtp/odtp-volume/data /odtp/odtp-workdir/data
-
-    # A3 - Run the tool
-    python3 -m synpp
-    #python3 -m synpp /odtp/odtp-workdir/scenario/config.yml
-
-    ## Matsim
-    #java -Xmx20G -cp ile_de_france_run.jar org.eqasim.ile_de_france.RunSimulation --config-path ile_de_france_config.xml
-
-    # Copying output in odtp-output
-    cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output
 
 elif [ "$SCENARIO" == "CORSICA" ]; then
     echo "CORSICA SCENARIO."
     
-    # Actions
-    # A1 - Clone github
+    # Cloning Scenario
     git clone https://github.com/eqasim-org/ile-de-france.git /odtp/odtp-workdir/scenario
     cd /odtp/odtp-workdir/scenario
     git checkout fb1112d2a7d1817746be84413da584c391059ad1
 
-    # A2A - Prepare parameters & Config File
-    # Read placeholders and create config file from Environment  
+    # Preparing parameters & config file
+    # Reading placeholders and create config file from environment variables
     if [ "$PIPELINE" == "Synthesis" ]; then
         echo "Running Synthesis PIPELINE"
         python3 /odtp/odtp-client/parameters.py /odtp/odtp-app/config_templates/config_synthesis_corsica.yml /odtp/odtp-workdir/scenario/config.yml
+    
+        # Preparing input data folder
+        ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
+    
     else
         echo "Running Matsim PIPELINE"
         python3 /odtp/odtp-client/parameters.py /odtp/odtp-app/config_templates/config_matsim_corsica.yml /odtp/odtp-workdir/scenario/config.yml
+
+        # Preparing input data folder
+        ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
+
+        # We need to copy output files from previous step in output folder
+        cp -r /odtp/odtp-input/eqasim-output/* /odtp/odtp-workdir/output
+
+        # Cache files are needed
+        cp -r /odtp/odtp-input/cache/* /odtp/odtp-workdir/cache
     fi
-
-    # A2B - Prepare datafolder
-    ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
-
-    # A3 - Run the tool
-    python3 -m synpp
-    #python3 -m synpp /odtp/odtp-workdir/scenario/config.yml
-
-    ## Matsim
-    #java -Xmx20G -cp ile_de_france_run.jar org.eqasim.ile_de_france.RunSimulation --config-path ile_de_france_config.xml
-
-    # Copying output in odtp-output
-    cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output
 
 else
     echo "CH SCENARIO."
 
-    # Actions
-    # A1 - Clone github
+    # Cloning Scenario
     git clone https://gitlab.ethz.ch/ivt-vpl/populations/ch-zh-synpop /odtp/odtp-workdir/scenario
     cd /odtp/odtp-workdir/scenario
     git switch develop
     git checkout 4658daa2e441dcda132622e7fcb47da1df8c47d6
     
-    # A2A - Prepare parameters & Config File
-    # Read placeholders and create config file from Environment  
+    # Preparing parameters & config file
+    # Reading placeholders and create config file from environment variables
     if [ "$PIPELINE" == "Synthesis" ]; then
         echo "Running Synthesis PIPELINE"
         python3 /odtp/odtp-client/parameters.py /odtp/odtp-app/config_templates/config_synthesis_ch.yml /odtp/odtp-workdir/scenario/config.yml
+    
+        # Preparing input data folder
+        ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
+    
     else
         echo "Running Matsim PIPELINE"
         python3 /odtp/odtp-client/parameters.py /odtp/odtp-app/config_templates/config_matsim_ch.yml /odtp/odtp-workdir/scenario/config.yml
+    
+        # Preparing input data folder
+        ln -s /odtp/odtp-input/data /odtp/odtp-workdir/data
+
+        # We need to copy output files from previous step in output folder
+        cp -r /odtp/odtp-input/eqasim-output/* /odtp/odtp-workdir/output
     fi
+fi
 
-    # A2B - Prepare datafolder
-    ln -s /odtp/odtp-volume/data /odtp/odtp-workdir/data
+# Running Eqasim pipeline
+python3 -m synpp
 
-    # A3 - Run the tool
-    python3 -m synpp
-    #python3 -m synpp /odtp/odtp-workdir/scenario/config.yml
+# Copying output in odtp-output
+mkdir /odtp/odtp-output/eqasim-output
+cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output/eqasim-output
 
-    ## Matsim
-    #java -Xmx20G -cp ile_de_france_run.jar org.eqasim.ile_de_france.RunSimulation --config-path ile_de_france_config.xml
-
-    # Copying output in odtp-output
-    cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output
+# Copying cache neccesary for running matsim.output
+if [ "$PIPELINE" == "Synthesis" ]; then
+    mkdir /odtp/odtp-output/cache
+    #cp /odtp/odtp-workdir/cache/pipeline.json /odtp/odtp-output/cache/pipeline.json
+    cp -r /odtp/odtp-workdir/cache/* /odtp/odtp-output/cache
 fi
 
